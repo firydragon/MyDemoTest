e04213974f1782d79bb520442ebf5424f38fd7c1
WuJianhui <710903155@qq.com>
2025-12-15 15:02:47 +0800

    第二次修改Main.java文件

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,14 @@ public class Main {
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
             System.out.println("测试修改！");
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
             // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-            System.out.println("i = " + i);
+            System.out.println("你是第" + i+"个用户，恭喜你中奖了！");
         }
     }
 }
