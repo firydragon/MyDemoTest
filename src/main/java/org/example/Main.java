5cae33879084f5600e145642162880ebd9628908
WuJianhui <710903155@qq.com>
2025-12-15 15:53:24 +0800

    增加输出内容

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("测试修改！");
             System.out.println(i);
+            System.out.println("冲突怎么还不出来");
+            System.out.println("太伤我心了");
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
             // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
             if (i==4){
                 System.out.println("你是第" + i+"个用户,恭喜您中奖了！");
             }
+            System.out.println(i);
             System.out.println("你是第" + i+"个用户，不好意思，您没有中奖");
         }
     }
